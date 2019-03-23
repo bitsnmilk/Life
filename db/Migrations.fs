ae52fe6231defccfde32f58c5362b57ede0c5dc9
Jeff Boek <jeff@jeffboek.com>
2019-03-23 15:58:32 -0700

    Adds basic tables

--- db/Migrations.fs
+++ db/Migrations.fs
@@ -2,29 +2,53 @@ module Migrations
 
 open FluentMigrator
 
-[<Migration(1L, "Adds user table")>]
+
+type Builders.Create.Table.ICreateTableColumnOptionOrWithColumnSyntax with
+    member x.WithCreatedAt() =
+        x.WithColumn("created_at").AsDateTime2().NotNullable().WithDefault(SystemMethods.CurrentDateTime)
+
+    member x.WithModifiedAt() =
+        x.WithColumn("modified_at").AsDateTime2().NotNullable().WithDefault(SystemMethods.CurrentDateTime)
+
+type Builders.Create.Table.ICreateTableWithColumnOrSchemaOrDescriptionSyntax with
+    member x.WithIdentityColumn() = x.WithColumn("id").AsInt64().PrimaryKey().Identity()
+
+    member x.WithCreatedAt() =
+        x.WithColumn("created_at").AsDateTime2().NotNullable().WithDefault(SystemMethods.CurrentDateTime)
+
+    member x.WithModifiedAt() =
+        x.WithColumn("modified_at").AsDateTime2().NotNullable().WithDefault(SystemMethods.CurrentDateTime)
+
+[<Migration(1L, "Adds base schema")>]
 type AddUserTable() =
     inherit Migration()
 
     override this.Up() =
         this.Create.Table("users")
-            .WithColumn("id").AsInt64().PrimaryKey().Identity()
-            .WithColumn("username").AsString().NotNullable()
-            .WithColumn("password").AsString().NotNullable() |> ignore
-    
+            .WithIdentityColumn()
+            .WithColumn("username").AsFixedLengthString(60).NotNullable()
+            .WithColumn("password").AsFixedLengthString(64).NotNullable()
+            .WithColumn("email").AsFixedLengthString(60).NotNullable()
+            .WithColumn("email_is_valid").AsBoolean().NotNullable().WithDefaultValue(false)
+            .WithColumn("user_enabled").AsBoolean().NotNullable().WithDefaultValue(false)
+            .WithColumn("bio").AsString().Nullable()
+            .WithCreatedAt().WithModifiedAt() |> ignore
+
+        this.Create.Table("posts")
+            .WithIdentityColumn()
+            .WithColumn("user_id").AsInt64().NotNullable()
+            .WithColumn("title").AsFixedLengthString(60).NotNullable()
+            .WithColumn("slug").AsFixedLengthString(60).NotNullable()
+            .WithColumn("description").AsFixedLengthString(150)
+            .WithColumn("post_type").AsInt16().NotNullable()
+            .WithColumn("markdown").AsString().Nullable()
+            .WithColumn("html").AsString().Nullable()
+            .WithColumn("body").AsString().Nullable()
+            .WithColumn("javascript").AsString().Nullable()
+            .WithCreatedAt().WithModifiedAt() |> ignore
+
+
+
     override this.Down() =
         this.Delete.Table("users") |> ignore
-   
-
-[<Migration(2L, "Adds posts table")>]
-type AddPostsTable() =
-    inherit Migration()
-
-    override this.Up() =
-        this.Create.Table("posts")
-            .WithColumn("id").AsInt64().PrimaryKey().Identity()
-            .WithColumn("title").AsString().NotNullable()
-            .WithColumn("body").AsString().NotNullable() |> ignore
-    
-    override this.Down() =
         this.Delete.Table("posts") |> ignore
