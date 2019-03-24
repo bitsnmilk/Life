c3d85780dd6c086eddb8692d594f31d942fea1af
Jeff Boek <jeff@jeffboek.com>
2019-03-24 10:25:55 -0700

    Bootstrap fable

--- src/client/src/App.fs
+++ src/client/src/App.fs
@@ -1,39 +1,67 @@
-module App
+module Client
+
+module Global =
+  type Page =
+    | Home
+    | Counter
+    | About
+
+  let toHash page =
+    match page with
+    | About -> "#about"
+    | Counter -> "#counter"
+    | Home -> "#home"
+
+module Types =
+  open Global
+
+  type Msg = Nothing
+
+  type Model = { currentPage : Page }
+
+module MyState =
+  open Elmish.Browser.Navigation
+  open Elmish.Browser.UrlParser
+  open Fable.Import.Browser
+  open Global
+  open Types
+
+  let pageParser : Parser<Page -> Page, Page> =
+    oneOf [ map About (s "about")
+            map Counter (s "counter")
+            map Home (s "home") ]
+
+  let urlUpdate result model =
+    match result with
+    | Some page -> ({ model with currentPage = page }, [])
+    | None ->
+        console.error ("Error parsing url")
+        (model, Navigation.modifyUrl (toHash model.currentPage))
+
+  let init result =
+    let (model, cmd) = urlUpdate result { currentPage = Home }
+    (model, cmd)
+
+  let update msg model = (model, [])
+
+module View =
+  open Fable.Helpers.React
+
+  let view model dispatch =
+    div [] [
+      h1 [] [str "Demo12234"]
+    ]
 
 open Elmish
+open Elmish.Browser.Navigation
+open Elmish.Browser.UrlParser
 open Elmish.React
-open Fable.Helpers.React
-open Fable.Helpers.React.Props
-
-// MODEL
-
-type Model = int
-
-type Msg =
-| Increment
-| Decrement
-
-let init() : Model = 0
-
-// UPDATE
-
-let update (msg:Msg) (model:Model) =
-    match msg with
-    | Increment -> model + 1
-    | Decrement -> model - 1
-
-// VIEW (rendered with React)
-
-let view (model:Model) dispatch =
-
-  div []
-      [ div [] [ str ("hello Jeff") ]
-        button [ OnClick (fun _ -> dispatch Increment) ] [ str "+" ]
-        div [] [ str (string model) ]
-        button [ OnClick (fun _ -> dispatch Decrement) ] [ str "-" ] ]
+open MyState
+open View
 
 // App
-Program.mkSimple init update view
+Program.mkProgram init update view
+|> Program.toNavigable (parseHash pageParser) urlUpdate
 |> Program.withReact "elmish-app"
 |> Program.withConsoleTrace
 |> Program.run
