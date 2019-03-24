22265d4dece24e920f7301303be1c5ed1fa23d10
Jeff Boek <jeff@jeffboek.com>
2019-03-24 11:34:17 -0700

    Adds some CSS

--- src/client/src/components/Footer.fs
+++ src/client/src/components/Footer.fs
@@ -1,15 +1,14 @@
 module Components.Footer
 
-open Domain
 open Fable.Helpers.React
 open Fable.Helpers.React.Props
 
 let view model dispatch =
-    header [] [
-        div [ ClassName "h-wrap" ] [
-            h1 [ ClassName "title" ] [
-                a [ Href "https://google.com"; Title "Back to Homepage" ] [ str "Bits N Milk" ]
-                a [ ClassName "menu-icon"; Title "Open Bio"] [ span [ ClassName "lines"] [] ]
-            ]
+    footer [] [
+        small [] [
+            str "Powered by Life"
+            str "- Theme: "
+            a [ Href "https://github.com/m3xm/hikari-for-Jekyll" ] [ str "hikari" ]
+            str " - &copy; Jeff Boek"
         ]
     ]
--- src/client/src/components/Header.fs
+++ src/client/src/components/Header.fs
@@ -7,8 +7,8 @@ let view model dispatch =
     header [] [
         div [ ClassName "h-wrap" ] [
             h1 [ ClassName "title" ] [
-                a [ Href "https://google.com"; Title "Back to Homepage" ] [ str "Bits N Milk" ]
-                a [ ClassName "menu-icon"; Title "Open Bio"] [ span [ ClassName "lines"] [] ]
+                a [ Href "https://google.com"; Title "Back to Homepage" ] [ str "bits n milk" ]
             ]
+            a [ ClassName "menu-icon"; Title "Open Bio"] [ span [ ClassName "lines"] [] ]
         ]
     ]
