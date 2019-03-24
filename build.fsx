0e37e5587e35b3d360ce5a5642244423985a18fe
Jeff Boek <jeff@jeffboek.com>
2019-03-23 21:00:41 -0700

    Scaffold the frontend

--- build.fsx
+++ build.fsx
@@ -3,8 +3,16 @@ nuget Fake.Core.Target
 nuget Fake.DotNet.Cli //"
 #load "./.fake/build.fsx/intellisense.fsx"
 
+
 open Fake.Core
+open Fake.IO
 open Fake.DotNet
+
+let serverPath = Path.getFullName "./src/server"
+let clientPath = Path.getFullName "./src/client"
+
+let run cmd args dir =
+    Shell.Exec(cmd, args, dir) |> ignore
 
 // Default target
 Target.create "Default" (fun _ ->
@@ -12,6 +20,12 @@ Target.create "Default" (fun _ ->
 
 Target.create "Migrate" (fun _ ->
   (DotNet.exec (DotNet.Options.withWorkingDirectory "./db") "run" "") |> ignore)
+
+Target.create "NPMInstall" (fun _ ->
+  run "npm" "install" clientPath)
+
+Target.create "RunClient" (fun _ ->
+  run "npm" "start" clientPath)
 
 // start build
 Target.runOrDefault "Default"
