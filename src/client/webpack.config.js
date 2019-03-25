5fa26eaae0be003289854bb70eaae163d2cf3679
Jeff Boek <jeff@jeffboek.com>
2019-03-24 17:04:35 -0700

    Adds scss

--- src/client/webpack.config.js
+++ src/client/webpack.config.js
@@ -1,20 +1,42 @@
 var path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+var commonPlugins = [
+    new HtmlWebpackPlugin({
+        filename: 'index.html',
+        template: './index.html',
+    })
+];
 
 module.exports = {
     mode: "development",
-    entry: "./src/Life.Client.fsproj",
+    entry: {
+        app: ["./src/Life.Client.fsproj"],
+        style: ["./scss/main.scss"]
+    },
     output: {
         path: path.join(__dirname, "./public"),
-        filename: "bundle.js",
+        filename: "[name].bundle.js",
     },
     devServer: {
         contentBase: "./public",
         port: 8087,
     },
+    plugins: commonPlugins,
     module: {
         rules: [{
             test: /\.fs(x|proj)?$/,
             use: "fable-loader"
+        },{
+            test: /\.(sass|scss|css)$/,
+            use: [
+                'style-loader',
+                'css-loader',
+                'sass-loader',
+            ]
+        }, {
+            test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+            loader: 'url-loader?limit=100000'
         }]
     }
 }
