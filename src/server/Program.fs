05fb0d9b376a7cf305f4f6f1001b8ecccf49e129
Jeff Boek <jeff@jeffboek.com>
2019-04-07 10:00:16 -0700

    Adds server

--- src/server/Program.fs
+++ src/server/Program.fs
@@ -1,11 +1,28 @@
 module Server
 
-open Suave
+open System
+open Giraffe
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Hosting
+open Microsoft.Extensions.DependencyInjection
 
-let config = { defaultConfig with
-                bindings = [ HttpBinding.createSimple HTTP "0.0.0.0" 8088 ] }
+let webApp =
+    choose [
+        route "/ping" >=> text "pong"
+        route "/" >=> text "home" ]
+
+let configureApp (app : IApplicationBuilder) =
+    app.UseGiraffe webApp
+
+let configureServices (services : IServiceCollection) =
+    services.AddGiraffe |> ignore
 
 [<EntryPoint>]
 let main argv =
-    startWebServer config (Successful.OK "Hello world")
+    WebHostBuilder()
+        .UseKestrel()
+        .Configure(Action<IApplicationBuilder> configureApp)
+        .ConfigureServices(configureServices)
+        .Build()
+        .Run()
     0 // return an integer exit code
